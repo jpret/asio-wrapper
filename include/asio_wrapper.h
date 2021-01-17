1e804d855f4bb227318891686a82d7f00cff9c3e
jangabriel <jangabriel@cppengineer.com>
2021-01-17 18:11:31 +0100

    Save commit for the day.

--- include/asio_wrapper.h
+++ include/asio_wrapper.h
@@ -15,19 +15,19 @@ namespace tcp {
 
 class Writable {
  public:
-  void Write(char* data, int len) = 0;
+  void Write(void* data, int len) = 0;
   virtual ~Writable = default;
 };
 
 class WritableHandler {
  public:
-  void HandleCallback(char* data, int len, std::shared_ptr<Writable>) = 0;
+  void HandleCallback(void* data, int len, std::shared_ptr<Writable>) = 0;
   virtual ~WritableHandler = default;
 };
 
 class Server: public Writable {
  public:
-  bool Initialise(int port);
+  bool Initialise(int port, WritableHandler &writable_handler);
   bool Start();
   bool Stop();
   void Write(char* data, int len) override;
@@ -35,7 +35,7 @@ class Server: public Writable {
 
 class Client: public Writable {
  public:
-  bool Initialise(int port, const std::string& address);
+  bool Initialise(int port, const std::string& address, WritableHandler& writable_handler);
   bool Start(int connect_timeout);
   bool Stop();
   void Write(char* data, int len) override;
